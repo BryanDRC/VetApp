88f5f9e1d395b8bb2732b1a0d4c6433dc4ac245b
YohanMora <53808272+YohanMora@users.noreply.github.com>
2023-11-15 20:46:50 -0600

    CambiosSprint3
    Se realizan cambios con respecto al sprint 3

--- VetApp/VetApp/VetApp/Entities/UserObj.cs
+++ VetApp/VetApp/VetApp/Entities/UserObj.cs
@@ -12,7 +12,7 @@
         public string UserPicture { get; set; } = "";
         public string UserPhoneNumber { get; set; } = string.Empty;
         public int IdRol { get; set; } = 0;
-        public int UserIdCard { get; set; } = 0;
+        public string UserIdCard { get; set; } = String.Empty;
         public string RolName { get; set; } = string.Empty;
     }
 }
