83dd6c44d45a5b9e22025010fd20a6df72be6f69
Bryan RC <98475591+BryanDRC@users.noreply.github.com>
2023-12-02 16:14:56 -0600

    Payment view
    .

---
+++ VetApp/VetApp/VetApp/Controllers/PaymentController.cs
@@ -0,0 +1,61 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VetApp.Entities;
+using VetApp.Models;
+using System.Drawing;
+using System.Text;
+using System.IO;
+
+
+namespace VetApp.Controllers
+{
+
+    [ResponseCache(NoStore = true, Duration = 0)]
+    public class PaymentController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ServiceModel _service;
+        public List<ServiceObj> _servicesObject;
+        private readonly ProductModel _productModel;
+        public List<ProductObj> _productsList;
+        private readonly ClientModel _clientModel;
+        public List<ClientObj> _clientList;
+
+        public PaymentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _service = new ServiceModel(configuration);
+            _servicesObject = _service.GetServices();
+            _productModel = new ProductModel(configuration);
+            _productsList = _productModel.GetProducts();
+            _clientModel = new ClientModel(configuration);
+            _clientList = _clientModel.GetClients();
+
+        }
+
+
+        public IActionResult Payment()
+        {
+
+            ViewBag.Services = _servicesObject;
+            ViewBag.Products = _productsList;
+            ViewBag.Clients = _clientList;
+            return View();
+        }
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
