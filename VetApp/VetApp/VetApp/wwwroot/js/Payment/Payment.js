d4e870e1b713613bb50dc99d715cf05eec844013
YohanMora <53808272+YohanMora@users.noreply.github.com>
2023-12-04 21:10:44 -0600

    Cambios factura
    Validaciones factura

--- VetApp/VetApp/VetApp/wwwroot/js/Payment/Payment.js
+++ VetApp/VetApp/VetApp/wwwroot/js/Payment/Payment.js
@@ -6,7 +6,18 @@ document.addEventListener('DOMContentLoaded', function () {
         var cantidadDescuentoInput = document.getElementById('productoDInput');
         var cantidadDescuento = parseFloat(cantidadDescuentoInput.value) || 0;
         var descuentoInputVal = parseFloat(document.getElementById('descuentoInput').value) / 100 || 0;
+        var descuentoInput = document.getElementById('descuentoInput');
+        var descuento = parseFloat(descuentoInput.value);
+        var cantidad = parseFloat(cantidadInput.value) || 0;
+        var productoDInput = document.getElementById('productoDInput');
+        var productosConDescuento = parseFloat(productoDInput.value) || 0;
+        var descuentoInput = document.getElementById('descuentoInput');
+        var descuento = parseFloat(descuentoInput.value) || 0;
+        var productoDInput = document.getElementById('productoDInput');
+        var productosConDescuento = parseFloat(productoDInput.value) || 0;
 
+
+        ////Validaciones
         if (insumosInput.value === "") {
             $('#alertaModal').modal('show');
             return;
@@ -17,6 +28,27 @@ document.addEventListener('DOMContentLoaded', function () {
             return;
         }
 
+
+        if (descuentoInput.value && (isNaN(descuento) || descuento < 1 || descuento > 100)) {
+            $('#descuentoInvalidoModal').modal('show');
+            return;
+        }
+
+
+        if (productoDInput.value && (isNaN(productosConDescuento) || productosConDescuento < 1 || productosConDescuento > cantidad)) {
+            $('#productosDescuentoInvalidoModal').modal('show');
+            return;
+        }
+
+
+        if (descuento > 0 && (!productoDInput.value || isNaN(productosConDescuento) || productosConDescuento <= 0)) {
+            $('#descuentoSinProductosModal').modal('show');
+            return;
+        }
+
+
+
+        //////Lógica de agregar filas a la tabla
         var servicio = insumosInput.options[insumosInput.selectedIndex].text;
         var instrucciones = document.getElementById('instruccionesInput').value;
         var precioUnitario = parseFloat(insumosInput.options[insumosInput.selectedIndex].getAttribute('data-precio')) || 0;
@@ -35,6 +67,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Crear botón de eliminar y añadir a la nueva fila
         var btnEliminar = document.createElement('button');
         btnEliminar.textContent = 'Eliminar';
+        btnEliminar.style.color = 'white';
         btnEliminar.style.color = 'white';
         btnEliminar.style.backgroundColor = 'red';
         btnEliminar.style.border = '1px solid red';
@@ -59,6 +92,8 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('ivaPInput').addEventListener('input', actualizarTotal);
     document.getElementById('ivaSInput').addEventListener('input', actualizarTotal);
 
+
+    //Lógica mostrar total y actualizar con IVAS
     function actualizarTotal() {
         var total = 0;
         var tabla = document.getElementById('miTabla').getElementsByTagName('tbody')[0];
@@ -77,10 +112,56 @@ document.addEventListener('DOMContentLoaded', function () {
 
         document.getElementById('totalpagar').value = total.toFixed(2);
         document.getElementById('totalFInput').value = totalFInput.toFixed(2);
-        document.getElementById('preciofinalInput').value = totalFInput.toFixed(2);
+
+        // Actualizar preciototalInput con el mismo valor que totalFInput
+        document.getElementById('preciototalInput').value = totalFInput.toFixed(2);
     }
 
-    
+
+    ////Lógica para mostrar campos distintos en tarjeta y efectivo
+    var tipoPagoInput = document.getElementById('tipopagoInput');
+    var divReferencia = document.getElementById('divReferencia');
+    var divCancelaCon = document.getElementById('divCancelaCon');
+    var divVuelto = document.getElementById('divVuelto');
+
+    tipoPagoInput.addEventListener('change', function () {
+        if (this.value === '2') {
+            divReferencia.style.display = 'block';
+            divCancelaCon.style.display = 'none';
+            divVuelto.style.display = 'none';
+        } else if (this.value === '1') {
+            divReferencia.style.display = 'none';
+            divCancelaCon.style.display = 'block';
+            divVuelto.style.display = 'block';
+        } else {
+            divReferencia.style.display = 'none';
+            divCancelaCon.style.display = 'none';
+            divVuelto.style.display = 'none';
+        }
+    });
+
+    // Función para calcular y actualizar el vuelto
+    function calcularVuelto() {
+        var total = parseFloat(preciototalInput.value) || 0;
+        var cancelado = parseFloat(canceladoInput.value) || 0;
+        var vuelto = cancelado - total;
+
+        // Asegurarse de que el vuelto no sea negativo
+        vueltoInput.value = vuelto > 0 ? vuelto.toFixed(2) : '0.00';
+    }
+
+    // Eventos para actualizar el vuelto cuando los valores cambien
+    preciototalInput.addEventListener('input', calcularVuelto);
+    canceladoInput.addEventListener('input', calcularVuelto);
+
+    // Inicializar el valor del vuelto al cargar la página
+    calcularVuelto();
+
+
+    //Validacion para la referencia
+    //document.getElementById('btnProcesar').addEventListener('click', function () {
+        
+    //});
 
 });
 
@@ -99,6 +180,16 @@ function getDetailsTable() {
 }
 
 function CreateInvoices() {
+
+    var tipoPago = document.getElementById('tipopagoInput').value;
+    var referencia = document.getElementById('referencia').value;
+
+    if (tipoPago === '2' && !referencia) {
+        $('#referenciaFaltanteModal').modal('show');
+        return;
+    } else {
+        console.log('Requisitos cumplidos');
+    }
 
     var detailInvoice = getDetailsTable();
 
